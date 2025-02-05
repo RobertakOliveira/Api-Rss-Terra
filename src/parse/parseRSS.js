74548c34b115e9918823c6609fa8611cd8eb5c7a
CarlosEduardo067 <carloseduardodossantosvital@gmail.com>
2025-02-05 14:40:09 -0400

    feat: implementando a conversão dos dados extraidos para JSON

--- src/parse/parseRSS.js
+++ src/parse/parseRSS.js
@@ -1,4 +1,5 @@
 import RSSParser from "rss-parser";
+import fs from 'fs'; 
 
 const feedUrl = "https://www.terra.com.br/rss.xml";
 
@@ -10,10 +11,22 @@ const parse = async url => {
     feed.items.forEach(item => {
         console.log(`${item.title} - ${item.link}\n${item.contentSnippet}\n\n`);
     });
+
+    return feed;
 };
 
-console.log("Parsing " + feedUrl);
-
-parse(feedUrl);
-
 //para rotar "node parseRSS.js" no terminal de caminho src/parse
+
+const saveToJson = (data, filename) => {
+    const jsonData = JSON.stringify(data, null, 2); // Converte para JSON com formatação
+    fs.writeFileSync(filename, jsonData); // Salva o JSON em um arquivo
+    console.log(`Dados salvos em ${filename}`);
+};
+
+const createFileJson = async () => {
+    console.log("Parsing " + feedUrl);
+    const feedData = await parse(feedUrl);
+    saveToJson(feedData, 'feed.json'); // Salva os dados em um arquivo JSON
+};
+
+createFileJson();
