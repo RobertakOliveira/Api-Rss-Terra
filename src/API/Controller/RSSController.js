609bc9658f76355e791b03befcea998c659ca0ce
Leonardo de Freitas Nogueira <leonogueira.nardo@gmail.com>
2025-02-06 18:11:49 -0300

    perf: Atualização do Controller, adicionando classe e ocultando chaves

--- src/API/Controller/RSSController.js
+++ src/API/Controller/RSSController.js
@@ -1,48 +1,48 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
-const __filename = fileURLToPath(import.meta.url);
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION, // 'us-east-1'
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,  // Obtém a chave de acesso da variável de ambiente
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,  // Obtém a chave secreta da variável de ambiente
+    }
+  });
+
 const __dirname = path.dirname(__filename); //Diretório atual
 const otherDirectory = path.join(__dirname, '..', '..', '/parse'); //Diretório aonde fica o arquivo feed.json
 
+class RSSController {
 
-// Configurações do S3
-const s3Client = new S3Client({
-    region: "us-east-1", 
-    credentials: {
-        accessKeyId: "",
-        secretAccessKey: "", 
-    },
-});
+    static async uploadFileToS3(req, res) {
+        const bucketName = "grupo-02";
+        const filePath = path.join(otherDirectory, "feed.json");
+        console.log(filePath);
+        const key = "feed";
 
-//Função para fazer o upload para o S3
-const uploadFileToS3 = async (bucketName, filePath, key) => {
-    try {
-        const fileContent = fs.readFileSync(filePath);
+        try {
+            const fileContent = fs.readFileSync(filePath);
 
-        
-        const params = {
-            Bucket: bucketName, 
-            Key: key, 
-            Body: fileContent,
-            ContentType: "application/json",
-        };
+            const params = {
+                Bucket: bucketName,
+                Key: key,
+                Body: fileContent,
+                ContentType: "application/json",
+            };
 
-        //Comando para dar o put no S3
-        const command = new PutObjectCommand(params);
-        await s3Client.send(command);
+            const command = new PutObjectCommand(params);
+            await s3Client.send(command);
 
-        console.log(`Arquivo ${key} enviado com sucesso para o bucket ${bucketName}.`);
-    } catch (error) {
-        console.error("Erro ao fazer upload do arquivo:", error);
+            res.send(`Arquivo ${key} enviado com sucesso para o bucket ${bucketName}.`);
+        } catch (error) {
+            res.status(500).send("Erro ao fazer upload do arquivo.");
+        }
     }
-};
+    
+}
 
-const bucketName = "grupo-02"; 
-const filePath = path.join(otherDirectory, "feed.json");
-console.log(filePath)
-const key = "feed";
-
-uploadFileToS3(bucketName, filePath, key);
+module.exports = RSSController;
