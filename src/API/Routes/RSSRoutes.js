5a587a3bf35f17db208e091dcebbe73b65734e21
Leonardo de Freitas Nogueira <leonogueira.nardo@gmail.com>
2025-02-10 16:36:02 -0300

    feat: Adição de rota para função de pegar conteúdo do S3

--- src/API/Routes/RSSRoutes.js
+++ src/API/Routes/RSSRoutes.js
@@ -4,5 +4,6 @@ import RSSController from '../Controller/RSSController.js';
 const router = express.Router();
 
 router.post('/upload', RSSController.uploadFileToS3);
+router.get('/download', RSSController.downloadFileFromS3);
 
 export default router;
