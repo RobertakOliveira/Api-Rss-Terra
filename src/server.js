247d7701daeb7a4eef75d0fa94626fc5d390938b
CarlosEduardo067 <carloseduardodossantosvital@gmail.com>
2025-02-10 17:28:31 -0400

    feat: implementação da configuração do cors para permitir requisições do Live Server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from './API/Routes/RSSRoutes.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors({origin: 'http://127.0.0.1:5500',}));
 
 app.use(express.json());
 app.use('/api', routes);
