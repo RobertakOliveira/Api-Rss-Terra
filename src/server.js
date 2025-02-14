3a238be1d9faefa643c5261746d59d6881cbef12
CarlosEduardo067 <carloseduardodossantosvital@gmail.com>
2025-02-14 10:58:20 -0400

    refactor: alterando para o frontend rodar junto da aplicação

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import routes from './API/Routes/RSSRoutes.js';
-import cors from 'cors';
+
+//Obtendo o diretório atual
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors({origin: 'http://127.0.0.1:5500',}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use('/api', routes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
